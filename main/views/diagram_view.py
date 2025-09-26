9e5e2c619b1f903f21c7b256a8a8a09a99f7b870
kabo <kbozikov@rao.hr>
2025-09-26 09:28:20 +0200

    PySide6 GUI dependency

---
+++ main/views/diagram_view.py
@@ -0,0 +1,1 @@
+from PySide6.QtWidgets import QGraphicsView, QGraphicsScene, QGraphicsRectItem, QGraphicsTextItem
---
+++ main/views/main_window.py
@@ -0,0 +1,2 @@
+from PySide6.QtWidgets import QApplication, QMainWindow, QListWidget, QSplitter, QTextEdit
+from PySide6.QtCore import Qt
